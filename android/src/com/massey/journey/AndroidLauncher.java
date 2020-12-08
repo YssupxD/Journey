58e66f2707366c5dc91a60df039b304b49dbb625
Jason Jiang <yssupxd@Jasons-MacBook-Pro.local>
2020-12-08 19:11:08 +1300

    Initial commit

---
+++ android/src/com/massey/journey/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.massey.journey;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.massey.journey.Journey;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Journey(), config);
+	}
+}
---
+++ desktop/src/com/massey/journey/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.massey.journey.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.massey.journey.Journey;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Journey(), config);
+	}
+}
