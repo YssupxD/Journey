d55f2e0b2e8841cebe26773eb9851726dae82b2e
yssupxd <jasotori0912>
2021-01-24 17:30:58 +1300

    jumping fixed

--- core/src/com/massey/journey/screens/MainGameScreen.java
+++ core/src/com/massey/journey/screens/MainGameScreen.java
@@ -6,27 +6,18 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.maps.MapObject;
-import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.massey.journey.Journey;
 import com.massey.journey.scenes.Hud;
 import com.massey.journey.sprites.Gino;
-import com.massey.journey.tools.B2WorldCreator;
-
-import static com.massey.journey.Journey.SCREEN_HEIGHT;
+import com.massey.journey.Utils.B2WorldCreator;
 
 public class MainGameScreen implements Screen {
 
@@ -48,6 +39,9 @@ public class MainGameScreen implements Screen {
     private World world;
     private Box2DDebugRenderer box2DDebugRenderer;
     private Gino gino;
+
+    //key pressed
+    public static int jPressed = 0;
 
     //constructor
     public MainGameScreen(Journey game) {
@@ -72,7 +66,7 @@ public class MainGameScreen implements Screen {
         gameCam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
 
         //Setup Box2D world, set 0 gravity in X axis, -10 in Y axis, and allow bodies to sleep
-        world = new World(new Vector2(0,-10), true);
+        world = new World(new Vector2(0,-9.8f), true);
         //allows for debug lines of game world
         box2DDebugRenderer = new Box2DDebugRenderer();
 
@@ -91,14 +85,17 @@ public class MainGameScreen implements Screen {
     }
 
     public void handleInput(float deltaTime) {
-        if(Gdx.input.isKeyPressed(Input.Keys.SPACE) || Gdx.input.isKeyPressed(Input.Keys.W)) {
-            gino.b2body.applyLinearImpulse(new Vector2(0, 0.7f), gino.b2body.getWorldCenter(), true);
+        if(Gdx.input.isKeyJustPressed(Input.Keys.SPACE) && gino.b2body.getLinearVelocity().y == 0){
+            gino.b2body.applyLinearImpulse(new Vector2(0, 3.5f), gino.b2body.getWorldCenter(),
+                    true);
         }
         if(Gdx.input.isKeyPressed(Input.Keys.D) && gino.b2body.getLinearVelocity().x <= 2) {
             gino.b2body.applyLinearImpulse(new Vector2(0.1f, 0), gino.b2body.getWorldCenter(), true);
         }
         if(Gdx.input.isKeyPressed(Input.Keys.A) && gino.b2body.getLinearVelocity().x >= -2) {
             gino.b2body.applyLinearImpulse(new Vector2(-0.1f, 0), gino.b2body.getWorldCenter(), true);
+        }
+        if(Gdx.input.isKeyJustPressed(Input.Keys.J)) {
         }
     }
 
