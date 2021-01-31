34491ca41212701dc24dcd8ee6fe08f01c2fe768
yssupxd <jasotori0912>
2021-01-31 23:30:07 +1300

    update 1.31

--- core/src/com/massey/journey/scenes/JoyCon.java
+++ core/src/com/massey/journey/scenes/JoyCon.java
@@ -1,16 +1,33 @@
 package com.massey.journey.scenes;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.Touchpad;
 import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class JoyCon implements Disposable {
     public Stage stage;
-    public Touchpad tPad;
     public OrthographicCamera cam;
+    private Viewport vp;
+    boolean isPressJump, isPressLeft, isPressRight, isPressThrow;
 
+    public JoyCon(SpriteBatch spriteBatch) {
+        cam = new OrthographicCamera();
+        vp = new FitViewport(800, 400, cam);
+        stage = new Stage(vp, spriteBatch);
+        Gdx.input.setInputProcessor(stage);
 
+        Table table = new Table();
+
+        //Image left = new Image(new Texture());
+    }
 
 
     @Override
